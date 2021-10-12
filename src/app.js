0f30c048f6731042cb8e369e344241b396f3bb44
Balakumar3018 <balakumarjkp@gmail.com>
2021-10-12 10:06:15 +0530

    docs-update docs

--- src/app.js
+++ src/app.js
@@ -4,20 +4,20 @@ import "./styles.css";
 export default function App() {
   const PhoneObjects = {
     Iphone: [
-      { name: "IPhone 13 pro ", rating: "5/5" },
-      { name: "IPhone 13 ", rating: "5/5" },
-      { name: "IPhone 13 mini", rating: "5/5" },
+      { name: "IPhone 13 pro ", Description : "Pro. Buy now. A dramatically more powerful camera system. Learn more. Ultra Wide camera. A15 Bionic chip. Cinematic mode. Adaptive 120Hz display. Super Retina XDR display.",rating: "5/5" },
+      { name: "IPhone 13 ",Description : "Your new superpower. Buy now. Our most advanced dual-camera system ever", rating: "5/5" },
+      { name: "IPhone 13 mini",Description : "13 cm (5.4-inch) Super Retina XDR display · Cinematic mode adds shallow depth of field and shifts focus automatically in your videos ", rating: "5/5" },
       
     ],
     Samsung: [
-      { name: "Samsung S21 ", rating: "4.7/5" },
-      { name: "Samsung S20 ", rating: "4.5/5" },
-      { name: "Samsung Z series", rating: "4/5" }
+      { name: "Samsung S21 ",Description : "Never miss that perfect shot again. Meet Galaxy S21 5G ", rating: "4.7/5" },
+      { name: "Samsung S20 ",Description : "Samsung Galaxy S20 · Released 2020, March 06 · 163g, 7.9mm thickness · Android 10, up to Android 11, One UI 3.0 · 128GB storage, microSDXC ...", rating: "4.5/5" },
+      { name: "Samsung Z series",Description : "Galaxy Z Series Mobiles - Discover Samsung Z Series mobile models online. ", rating: "4/5" }
     ],
     Oneplus: [
-      { name: "Oneplus 9T ", rating: "4.3/5" },
-      { name: "Oneplus 9 ", rating: "4.2/5" },
-      { name: "Oneplus nord 2", rating: "3.8/5" }
+      { name: "Oneplus 9 ",Description : "This festive season , capture your best shot with OnePlus 9 Series 5G starting from 34,999 ", rating: "4.3/5" },
+      { name: "Oneplus 8T ",Description : "OnePlus 8T 5G is equipped with Qualcomm® Snapdragon™ 865 and 5G, 48 MP Quad Camera", rating: "4.2/5" },
+      { name: "Oneplus nord 2", Description : "Check pricing and color options for OnePlus Nord 2 5G, review product info, and purchase a new Nord 2 phone.",rating: "3.8/5" }
     ]
   };
   let [PhoneModel, SetphoneModel] = useState("Iphone");
@@ -66,6 +66,7 @@ export default function App() {
             >
               {" "}
               <div style={{ fontSize: "larger" }}> {phone.name} </div>
+              <div style={{ fontSize: "small" }}> {phone.Description} </div>
               <div style={{ fontSize: "smaller" }}> {phone.rating} </div>
             </li>
           ))}
